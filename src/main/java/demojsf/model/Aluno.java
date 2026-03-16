e1b1bbc6e60c961dc1a57309f67a3a669bbc9ff9
Murakami <murakami.edson@gmail.com>
2026-03-16 18:16:49 -0300

    first commit

---
+++ src/main/java/demojsf/model/Aluno.java
@@ -0,0 +1,14 @@
+package demojsf.model;
+
+
+import lombok.Data;
+
+@Data
+public class Aluno {
+
+	private Integer id;
+
+	private String nome;
+	private String email;
+
+}
---
+++ src/main/java/demojsf/service/AlunoService.java
@@ -0,0 +1,19 @@
+package demojsf.service;
+
+import java.io.Serializable;
+import java.util.List;
+
+import demojsf.model.Aluno;
+
+
+public class AlunoService implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	public List<Aluno> buscarTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+}
